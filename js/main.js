e4d472d8548ed16a8a5d6d7e4a4b9c4c3d2775a7
Born0 <bornoahmed2@gmail.com>
2025-07-05 01:48:30 +0600

    checkout v1, wishlist v1, store v2

--- js/main.js
+++ js/main.js
@@ -79,25 +79,25 @@
 //   });
 
 	// Product imgs Slick
-  $('#product-imgs').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    centerMode: true,
-    focusOnSelect: true,
-		centerPadding: 0,
-		vertical: true,
-    asNavFor: '#product-main-img',
-		responsive: [{
-        breakpoint: 991,
-        settings: {
-					vertical: false,
-					arrows: false,
-					dots: true,
-        }
-      },
-    ]
-  });
+//   $('#product-imgs').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     arrows: true,
+//     centerMode: true,
+//     focusOnSelect: true,
+// 		centerPadding: 0,
+// 		vertical: true,
+//     asNavFor: '#product-main-img',
+// 		responsive: [{
+//         breakpoint: 991,
+//         settings: {
+// 					vertical: false,
+// 					arrows: false,
+// 					dots: true,
+//         }
+//       },
+//     ]
+//   });
 
 	// Product img zoom
 	var zoomMainProduct = document.getElementById('product-main-img');
